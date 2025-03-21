6119022e6b8c25c7442ef9518655765bc969284f
Umang Kumar <umangk1@kpit.com>
2025-03-21 11:48:41 +0530

    First commint

---
+++ src/test/java/com/umang/jobapp/JobappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umang.jobapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
